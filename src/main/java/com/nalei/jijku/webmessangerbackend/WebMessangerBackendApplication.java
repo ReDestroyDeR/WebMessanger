0987e6c1c0f377150c098d377127cc1a18a1eda5
Daniil Shreyder <d.shreyder@gaskar.group>
2022-10-08 00:41:51 +0300

    Initial commit

---
+++ src/main/java/com/nalei/jijku/webmessangerbackend/WebMessangerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nalei.jijku.webmessangerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMessangerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebMessangerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nalei/jijku/webmessangerbackend/WebMessangerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nalei.jijku.webmessangerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMessangerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
